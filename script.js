529666a61e299571be00fc409878d30ef87cfa62
Krishna Badole <badolek23@gamil.com>
2025-09-29 22:33:04 +0530

    Your commit message

---
+++ script.js
@@ -0,0 +1,33 @@
+let display=document.querySelector(".display");
+function append(value){
+    display.value +=value;
+}
+
+function allClear(){
+    display.value=" ";
+}
+function deleteLast(){
+    display.value=display.value.slice(0,-1);
+}
+function calculate() {
+    try{
+        display.value=eval(display.value.replace('%','/100'));
+    }
+    catch{
+    display.value=Error;
+    }
+}
+document.addEventListener('keydown', function (e) {
+    const key = e.key;
+
+    if (!isNaN(key) || ['+', '-', '*', '/', '.', '%'].includes(key)) {
+        append(key);
+    } else if (key === 'Enter' || key === '=') {
+        e.preventDefault();
+        calculate();
+    } else if (key === 'Backspace') {
+        deleteLast();
+    } else if (key === 'Escape') {
+        clearDisplay();
+    }
+});
